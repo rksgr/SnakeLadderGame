e0c70d7b5d18a89fb961453625360927b8272362
rksgr <r_kjha@yahoo.com>
2021-12-11 11:03:50 +0530

    Ensure player gets to exact position 100

---
+++ SnakeLadderGameUC5.java
@@ -0,0 +1,43 @@
+
+package com.mycompany.snake_ladder_08_dec;
+// Ensure the player gets to exact winning position 100
+public class SnakeLadderGameUC5 {
+    public static void main(String []args){
+        //constants
+        final int Start_POS = 0;
+        final int FINAL_POS = 100;
+        // variables
+        int new_pos = Start_POS;
+        while (new_pos !=FINAL_POS){
+            // Get random integer between 1 and 6
+            int roll_num = (int)Math.ceil(Math.random()*6);
+            // Get random integer between 0 and 2
+            int options_avail = (int)Math.floor(Math.random()*3);
+            //System.out.println("The game starts from the starting position of "+ START_POS);
+            System.out.println("The player gets the number "+ roll_num);
+            // Moves behind
+            if (options_avail==0){
+                new_pos -=roll_num;
+                System.out.println("The player moves behind by "+ roll_num + " positions"
+                            + "and reaches the position of "+ new_pos);
+            }
+            // Does not move
+            else if (options_avail == 1){
+
+                    System.out.println("The player does not move.");
+            }
+            // Moves ahead
+            else if (options_avail==2){
+                    new_pos +=roll_num;
+                    System.out.println("The player moves ahead by "+ roll_num + " positions and reaches the position of "
+                            + new_pos); 
+            }
+            if (new_pos<0){
+                new_pos = 0;
+            }
+            if (new_pos>100){
+                new_pos -=roll_num;
+            }
+        }   
+    }
+}
