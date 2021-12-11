c29ad9651a591c41bec31b4bed7f1536a2efd34e
rksgr <r_kjha@yahoo.com>
2021-12-11 10:59:25 +0530

    Single Player starts position 0

---
+++ SnakeLadderGameUC1.java
@@ -0,0 +1,9 @@
+package com.mycompany.snake_ladder_08_dec;
+
+public class SnakeLadderGameUC1 {
+    public static void main(String []args){
+        int START_POS = 0;
+        System.out.println("Welcome to the interesting game of Snake and Ladder!");
+        System.out.println("Your starting position is zero");
+    }
+}
